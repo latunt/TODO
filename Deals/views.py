65d7bdeded4ed4a617a9d2eec942796571b8b01a
LATUNT <vzalucaev@gmail.com>
2022-10-29 21:28:24 +0300

    add path

--- Deals/views.py
+++ Deals/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
 # Create your views here.
+def signup(request):
+    if request.method=="GET":
+        return render(request,'Deals/signup.html')
+    else:
+        pass
