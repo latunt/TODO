cb9d10c5da4160ba85adab84202ca9fcf435c409
LATUNT <vzalucaev@gmail.com>
2022-10-30 10:13:25 +0300

    Add user

--- Deals/views.py
+++ Deals/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login
 # Create your views here.
 def signup(request):
     if request.method=="GET":
-        return render(request,'Deals/signup.html')
+        return render(request,'Deals/signup.html',{'form':UserCreationForm})
     else:
-        pass
+        if request.POST(['password1'])==request.POST(['password2']):
